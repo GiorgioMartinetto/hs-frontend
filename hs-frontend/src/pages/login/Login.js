9d015fb4d72fac988000963b0e9a119f5fd68176
GiorgioMartinetto <giorgio.martinetto@outlook.it>
2023-04-13 17:57:20 +0200

    prova di git

--- hs-frontend/src/pages/login/Login.js
+++ hs-frontend/src/pages/login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
 
     return(
         <>
-            <div className='login-container'>
+            <div className='login-container peppereppe'>
                 <div className="container text-center">
                     <div className="row">
                         <div className="col">
